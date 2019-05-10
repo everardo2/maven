a5b357a1a3283f7dcee98b427113b5723527a6a7
everardo2 <49213012+everardo2@users.noreply.github.com>
2019-05-10 14:20:08 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/hello_worl/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.hello_worl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
